e3dae6263c434c01f845774197bc3982092e1861
Jacobo-Vargas <126033979+Jacobo-Vargas@users.noreply.github.com>
2023-08-10 00:30:34 -0500

    update

---
+++ src/main/java/model/Producto.java
@@ -0,0 +1,49 @@
+package model;
+
+public class Producto {
+    private final String codigoProducto;
+    private final String nombreProducto;
+    private String descripcionProducto;
+    private float valorUnitario;
+    private int cantidadExistente;
+
+    public Producto(String codigoProducto, String nombreProducto, String descripcionProducto, float valorUnitario, int cantidadExistente) {
+        this.codigoProducto = codigoProducto;
+        this.nombreProducto = nombreProducto;
+        this.descripcionProducto = descripcionProducto;
+        this.valorUnitario = valorUnitario;
+        this.cantidadExistente = cantidadExistente;
+    }
+
+    public String getCodigoProducto() {
+        return codigoProducto;
+    }
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public String getDescripcionProducto() {
+        return descripcionProducto;
+    }
+
+    public void setDescripcionProducto(String descripcionProducto) {
+        this.descripcionProducto = descripcionProducto;
+    }
+
+    public float getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(float valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    public int getCantidadExistente() {
+        return cantidadExistente;
+    }
+
+    public void setCantidadExistente(int cantidadExistente) {
+        this.cantidadExistente = cantidadExistente;
+    }
+}
