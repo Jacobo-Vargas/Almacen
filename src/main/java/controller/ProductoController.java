401ac056b932672948ce39e00e8c837faaae1ec3
Juan-Pablo-UQ <126034289+Juan-Pablo-UQ@users.noreply.github.com>
2023-08-17 21:15:44 -0500

    ventana controller
    se agregaron atributos en el ventana controller

--- src/main/java/controller/ProductoController.java
+++ src/main/java/controller/ProductoController.java
@@ -1,4 +1,54 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class ProductoController {
+    @FXML
+    public Button BotonAgregar;
+    @FXML
+    public Button BotonBuscar;
+    @FXML
+    public Button BotonEliminar;
+    @FXML
+
+    public TextField TextFieldCodigo;
+    @FXML
+    public TextField TextFieldDescripcion;
+    @FXML
+    public TextField TextFieldValor;
+    @FXML
+    public TextField TextFieldNombre;
+    @FXML
+    public TextField TextFieldExistencia;
+    @FXML
+    public TextField TextFieldFechaEnvasado;
+    @FXML
+    public TableView TableViewTablaMostrar;
+    @FXML
+    public TextField TextFieldPeso;
+    @FXML
+    public TextField TextFieldFechaVencimiento;
+    @FXML
+    public TextField TextFieldCodigoAprobacion;
+    @FXML
+    public TextField TextFieldTemratura;
+    @FXML
+    public ComboBox ComboBoxPais;
+    @FXML
+    public ComboBox ComboBoxTipoProducto;
+    @FXML
+
+    public TableColumn TableColumnCodigo;
+    @FXML
+    public TableColumn TableColumnNombre;
+    @FXML
+    public TableColumn TableColumnDescripcion;
+    @FXML
+    public TableColumn TableColumnValor;
+    @FXML
+    public TableColumn Existencia;
+    public void initialize(){
+
+    }
 }
