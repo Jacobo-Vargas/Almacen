9708a08d413e0bd5f822c48464cb78103dbf3ff7
jacobo <jacobo.vargasg@uqvirtual.edu.co>
2023-08-21 16:28:52 -0500

    se arregla el metodo descontar productos al concluir la venta

--- src/main/java/controller/VentasController.java
+++ src/main/java/controller/VentasController.java
@@ -71,7 +71,7 @@ public class VentasController {
         AlmacenInstance.INSTANCE.getAlmacen().registrarCliente(new ClientePersonaJuridica("Jacobo", "Vargas", "García", "1094958613", "Cerros del viento", "3186569265", "1094958613-1"));
         AlmacenInstance.INSTANCE.getAlmacen().registrarCliente(new ClientePersonaNatural("Juan", "Buitrago", "piragua", "1234567890", "Barrio quindio", "3112360897", "juan123@gmail.com", LocalDate.of(2004, 2, 23)));
 
-        AlmacenInstance.INSTANCE.getAlmacen().registrarProducto(new ProductoRefrigerado("02", "helado", "Helado sabor a fresa", 2900, 25, "A01", -18));
+        AlmacenInstance.INSTANCE.getAlmacen().registrarProducto(new ProductoRefrigerado("04", "helado", "Helado sabor a fresa", 2900, 25, "A01", -18));
         AlmacenInstance.INSTANCE.getAlmacen().registrarProducto(new ProductoPerecedero("03", "atun", "Atun en aceite", 7500, 15, LocalDate.of(2024, 5, 24)));
 
 
